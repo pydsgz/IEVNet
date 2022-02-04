0298a59d975c599933aa8b17f83a9ab25ba9a968
Seyed-Ahmad Ahmadi <mangotee@gmail.com>
2022-02-04 11:35:25 +0100

    Converted line endings in start_docker.sh from Win to Linux format.

--- start_docker.sh
+++ start_docker.sh
@@ -3,4 +3,7 @@ docker run \
     --ipc=host \
     -p=9999:9999 \
     --name="monai_030_rc4" \
-    -v ./:/ievnet/ projectmonai/monai:0.3.0rc4 /bin/bash
+    -v $(pwd)/:/ievnet/ \
+    projectmonai/monai:0.3.0rc4 \
+    /bin/bash
+
